991b3b5b6a5053dd046e38110c298a082a93c173
peterlyeung <peter.yeung@gmail.com>
2024-05-04 15:41:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# crewai_researcher
+ 
---
+++ main.py
@@ -0,0 +1,96 @@
+import os
+from crewai import Agent, Task, Crew, Process
+# from crewai_tools import SerperDevTool
+from langchain.llms import Ollama
+
+# os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
+# os.environ["SERPER_API_KEY"] = "Your Key" # serper.dev API key
+
+ollama_llm = Ollama(model="openhermes")
+mistral_llm = Ollama(model="mistral")
+# You can choose to use a local model through Ollama for example. See https://docs.crewai.com/how-to/LLM-Connections/ for more information.
+
+# os.environ["OPENAI_API_BASE"] = 'http://localhost:11434/v1'
+# os.environ["OPENAI_MODEL_NAME"] ='openhermes'  # Adjust based on available model
+# os.environ["OPENAI_API_KEY"] ='sk-111111111111111111111111111111111111111111111111'
+
+from langchain.tools import DuckDuckGoSearchRun
+
+from crewai_tools import tool
+
+@tool('DuckDuckGoSearch')
+def ducksearch(q: str):
+    """Search the web for information on a given topic"""
+    return DuckDuckGoSearchRun().run(q)
+
+# search = DuckDuckGoSearchRun()
+# search_tool = Tool(
+#     name="search\_tool",
+#     description="A search tool used to query DuckDuckGo for search results when trying to find information from the internet.",
+#     func=search.run
+# )
+
+
+# Define your agents with roles and goals
+researcher = Agent(
+  role='Senior Research Analyst',
+  goal='Find the latest topic about AI on the internet',
+  backstory="""You work at a leading tech think tank.
+  Your expertise lies in identifying emerging trends.
+  You have a knack for dissecting complex data and presenting actionable insights.""",
+  verbose=True,
+  allow_delegation=False,
+  tools=[ducksearch],
+  llm=ollama_llm
+  # You can pass an optional llm attribute specifying what model you wanna use.
+  # It can be a local model through Ollama / LM Studio or a remote
+  # model like OpenAI, Mistral, Antrophic or others (https://docs.crewai.com/how-to/LLM-Connections/)
+  #
+  # import os
+  # os.environ['OPENAI_MODEL_NAME'] = 'gpt-3.5-turbo'
+  #
+  # OR
+  #
+  # from langchain_openai import ChatOpenAI
+  # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7)
+)
+writer = Agent(
+  role='Tech Content Strategist',
+  goal='Craft compelling content on tech advancements',
+  backstory="""You are a renowned Content Strategist, known for your insightful and engaging articles.
+  You transform complex concepts into compelling narratives.""",
+  verbose=True,
+  allow_delegation=True,
+  llm=mistral_llm
+)
+
+# Create tasks for your agents
+task1 = Task(
+  description="""Conduct a comprehensive analysis of the latest advancements in AI in 2024.
+  Identify key trends, breakthrough technologies, and potential industry impacts.""",
+  expected_output="Full analysis report in bullet points",
+  agent=researcher,
+  tools=[ducksearch]
+)
+
+task2 = Task(
+  description="""Using the insights provided, develop an engaging blog
+  post that highlights the most significant AI advancements.
+  Your post should be informative yet accessible, catering to a tech-savvy audience.
+  Make it sound cool, avoid complex words so it doesn't sound like AI.""",
+  expected_output="Full blog post of at least 4 paragraphs",
+  agent=writer
+)
+
+# Instantiate your crew with a sequential process
+crew = Crew(
+  agents=[researcher, writer],
+  tasks=[task1, task2],
+  verbose=2, # You can set it to 1 or 2 to different logging levels
+)
+
+# Get your crew to work!
+result = crew.kickoff()
+
+print("######################")
+print(result)
